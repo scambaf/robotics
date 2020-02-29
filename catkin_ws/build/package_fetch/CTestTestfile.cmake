abd185287bea4910457bdb6b6be16f8b082aaeea
scambaf <samu_laluz@hotmail.com>
2020-02-29 20:20:59 +0100

    Paquete package_fetch (Lección 32)

---
+++ catkin_ws/build/package_fetch/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/samu/robotics/catkin_ws/src/package_fetch
+# Build directory: /home/samu/robotics/catkin_ws/build/package_fetch
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
